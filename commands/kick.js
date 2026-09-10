401209383034222f2e84c57e936308611375b63f
Alp <huwesreal16@gmail.com>
2026-09-10 14:40:49 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,10 +1,6 @@
-const {
-  SlashCommandBuilder,
-  PermissionFlagsBits,
-  MessageFlags,
-} = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
 const { addCeza, addLog } = require('../utils/db');
-const { COLOR, timestamp } = require('../utils/cv2');
+const { COLOR } = require('../utils/cv2');
 const { errContainer, dmContainer, modAction } = require('./ban');
 
 module.exports = {
@@ -27,20 +23,11 @@ module.exports = {
       return interaction.reply({ flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral, components: [errContainer('Kullanıcı sunucuda değil.')] });
 
     try {
-      await user.send({
-        flags: MessageFlags.IsComponentsV2,
-        components: [dmContainer(interaction.guild.name, interaction.guild.iconURL(), 'kick', sebep, interaction.user.tag)],
-      }).catch(() => {});
-
+      await user.send({ flags: MessageFlags.IsComponentsV2, components: [dmContainer(interaction.guild.name, 'kick', sebep, interaction.user.tag)] }).catch(() => {});
       await member.kick(`[huw3s Guard] ${sebep} | Mod: ${interaction.user.tag}`);
       addCeza(interaction.guild.id, user.id, interaction.user.id, 'kick', sebep);
-      addLog(interaction.guild.id, 'ban', `Kick: ${user.tag}`, `Sebep: ${sebep}`,
-        interaction.user.id, interaction.user.tag, user.id, user.tag, '#f59f00');
-
-      await interaction.reply({
-        flags: MessageFlags.IsComponentsV2,
-        components: [modAction('👢  Kullanıcı Atıldı', COLOR.warn, user, interaction.user, sebep)],
-      });
+      addLog(interaction.guild.id, 'ban', `Kick: ${user.tag}`, `Sebep: ${sebep}`, interaction.user.id, interaction.user.tag, user.id, user.tag, '#f59f00');
+      await interaction.reply({ flags: MessageFlags.IsComponentsV2, components: [modAction('👢  Kullanıcı Atıldı', COLOR.warn, user, interaction.user, sebep)] });
     } catch (e) {
       await interaction.reply({ flags: MessageFlags.IsComponentsV2 | MessageFlags.Ephemeral, components: [errContainer(`Kick başarısız: ${e.message}`)] });
     }
